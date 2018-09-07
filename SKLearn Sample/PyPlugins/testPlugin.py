a6d61ec3cb50e70b4916487fc20dabcecbc9f313
Antony Marsden <antony_marsden@hotmail.com>
2018-09-07 07:49:27 +0100

    Removed some old unused debug code

--- SKLearn Sample/PyPlugins/testPlugin.py
+++ SKLearn Sample/PyPlugins/testPlugin.py
@@ -4,7 +4,6 @@ Created on 4 Sep 2018
 @author: ANTM
 '''
 
-import numpy as np
 from sklearn.covariance import EllipticEnvelope
 from sklearn.svm import OneClassSVM
 from sklearn.datasets import load_boston
@@ -16,18 +15,13 @@ class TestPlugin(EPLPluginBase):
         super(TestPlugin,self).__init__(init)
         # Define "classifiers" to be used
         self.classifiers = {
-            "Empirical Covariance": EllipticEnvelope(support_fraction=1.,
-                                                     contamination=0.261),
-            "Robust Covariance (Minimum Covariance Determinant)":
-            EllipticEnvelope(contamination=0.261),
-            "OCSVM": OneClassSVM(nu=0.261, gamma=0.05)}
+            "Empirical Covariance": EllipticEnvelope(support_fraction=1., contamination=0.261),
+            "Robust Covariance (Minimum Covariance Determinant)": EllipticEnvelope(contamination=0.261),
+            "OCSVM": OneClassSVM(nu=0.261, gamma=0.05)
+        }
         
         # Get data
-        self.TrainingData = load_boston()['data'][:, [8, 10]]  # two clusters
-        # Keep this for drawing graphs with future data
-        
-        self.Data = []#self.TrainingData.copy()
-        self.IsOutlier = []
+        self.TrainingData = load_boston()['data'][:, [8, 10]]
         
     
     @EPLAction("action<>")
@@ -39,18 +33,12 @@ class TestPlugin(EPLPluginBase):
     @EPLAction("action<apama.test.HousingData> returns dictionary<string, boolean>")
     def CheckIfOutlier(self, d):
         asData = [d.fields["RAD"], d.fields["PTRATIO"]]
-        if len(self.Data) == 0:
-            self.Data = [asData]
-        else:
-            self.Data = np.append(self.Data, [asData], axis=0)
-
         res = {}
         for (clf_name, clf) in self.classifiers.items():
+            # Test this data point against our classifier
             predictions = clf.predict([asData])
-            # This is numpy.bool by default
+            # This is numpy.bool_ by default, since predictions[0] is a numpy.int32 or int64. Cast so EPL understands it
             entry = {clf_name : bool(predictions[0] == -1)}   # -1 means outlier, 1 means inlier
             res.update(entry)
-        
-        self.IsOutlier.append(res)
         
         return res
